f2dfea3df77260afd79431f2a8c896f60fe6aa2c
michaelcafiero99 <78000917+michaelcafiero99@users.noreply.github.com>
2023-04-06 18:15:57 -0500

    msg

--- main/postgres.py
+++ main/postgres.py
@@ -1,11 +1,21 @@
 import psycopg2
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+DB_NAME = os.getenv('DB_NAME')
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
 
 # Set up a connection to the database
 conn = psycopg2.connect(
-    host="localhost",
-    database="postgres",
-    user="postgres",
-    password="punkinbunker"
+    host=DB_HOST,
+    database=DB_NAME,
+    user=DB_USER,
+    password=DB_PASSWORD
 )
 
 # Open a cursor to perform database operations
