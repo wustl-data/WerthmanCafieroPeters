0f1d60fb07c3df5b5273e990eee6e07eb7377c13
Ty Schlichenmeyer <tyschlich@gmail.com>
2023-04-26 07:58:41 +0000

    clean up models

--- main/my_project/models/5yearmodel.sql
+++ main/my_project/models/5yearmodel.sql
@@ -1,2 +1,4 @@
-SELECT * FROM commodities2 WHERE month >= TO_DATE('2018-01-01', 'YYYY-MM-DD')
+SELECT * 
+FROM {{ source('5year', 'commodities2') }} 
+WHERE month >= TO_DATE('2018-01-01', 'YYYY-MM-DD')
 ORDER BY month DESC
